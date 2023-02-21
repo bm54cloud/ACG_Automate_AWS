ddfbd3c65f7a47776a88befc508d0f750e2aebde
Brandi McCall <bm54cloud@gmail.com>
2023-02-21 23:47:37 +0000

    feb 21

--- W15_foundational_datetime_SQS_function.py
+++ W15_foundational_datetime_SQS_function.py
@@ -6,12 +6,18 @@ import json
 
 # Lambda function
 def lambda_handler(event, context):
-    utc_ts = datetime.datetime.now().timestamp() # Gets current date/time in UTC format (string)
-    utc_hr = time.ctime(utc_ts) # Converts UTC format string to human readable format
-    sqs = boto3.client('sqs', region_name = 'us-east-2') # Get the service client
-    sqs.send_message( # Action to send message to SQS
-        QueueUrl = "https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS", # URL of the SQS queue you want to send a message to
-        MessageBody = utc_hr) # Contents of the message you want to send
+    # Get current date/time in UTC format (string)
+    utc_ts = datetime.datetime.now().timestamp() 
+    # Convert UTC format string to human readable format
+    utc_hr = time.ctime(utc_ts) 
+    # Get the service client
+    sqs = boto3.client('sqs', region_name = 'us-east-2') 
+    # Action to send message to SQS
+    sqs.send_message( 
+        # URL of the SQS queue you want to send a message to
+        QueueUrl = "https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS", 
+        # Contents of the message you want to send
+        MessageBody = utc_hr) 
     return {
         'statusCode': 200,
         'body': json.dumps(utc_hr)
