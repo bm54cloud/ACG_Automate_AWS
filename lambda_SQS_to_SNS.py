e2e2b10bdcde87b133049fa6676a75eda8bcdcd5
Brandi McCall <bm54cloud@gmail.com>
2023-02-23 03:10:47 +0000

    feb 22

--- lambda_SQS_to_SNS.py
+++ lambda_SQS_to_SNS.py
@@ -5,20 +5,21 @@ import json
 # Lambda function
 #def lambda_handler(event, context):
 
-    # Create SQS client and adjust region as needed
+#THIS WORKS
+# Create SQS client and adjust region as needed
+# for loop and interate through, call publish on each interation
 sqs = boto3.client('sqs', region_name = 'us-east-2') 
-    # Receive message from SQS
-response = sqs.receive_message(
-    QueueUrl = 'https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS')
-print(response)
-#message = response['Messages']
-#print(message)
+# Receive message from SQS using URL of the queue
+response = sqs.receive_message(QueueUrl = 'https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS_2')
 
-    
-   
-    
+message = response['Messages'][0]['Body']
+print(message) # This will print to CloudWatch
+
+# make this into a function that takes string value and call publish 
 # Send message to SNS topic
-#sns = boto3.client('sns', region_name = 'us-east-2')
+sns = boto3.client('sns', region_name = 'us-east-2')
     
-#pub_mess = sns.publish(TopicArn = 'arn:aws:sns:us-east-2:925367513330:W15_SNS_topic'
-    ##Message = )
+pub_mess = sns.publish(
+    TopicArn = 'arn:aws:sns:us-east-2:925367513330:W15_SNS_topic', 
+    Message = message)
+print(pub_mess)
--- test.py
+++ test.py
@@ -1,36 +1,9 @@
-#WORKS but does not convert UTC time to MST
 import boto3
-import datetime 
-import time
-import json
 
-def lambda_handler(event, context):
-    utc_ts = datetime.datetime.now().timestamp()
-    utc_hr = time.ctime(utc_ts)
-    sqs = boto3.client('sqs', region_name = 'us-east-2')
-    sqs.send_message(
-        QueueUrl = "https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS",
-        MessageBody = utc_hr)
-    return {
-        'statusCode': 200,
-        'body': json.dumps(utc_hr)
-    }
+sqs = boto3.resource('sqs', region_name = 'us-east-2')
 
-# WORKS in Cloud9, not Lambda
-import boto3
-import datetime 
-import time
-import pytz
-import json
+queue = sqs.create_queue(
+  QueueName='W15_SQS_2', 
+  Attributes={'DelaySeconds': '5', 'VisibilityTimeout': '60'})
 
-def lambda_handler(event, context):
-    utc_ts = datetime.datetime.now(pytz.utc)
-    mst_ts = utc_ts.astimezone(pytz.timezone('US/Mountain'))
-    sqs = boto3.client('sqs', region_name = 'us-east-2')
-    sqs.send_message(
-        QueueUrl = "https://sqs.us-east-2.amazonaws.com/925367513330/W15_SQS",
-        MessageBody = mst_ts)
-    return {
-        'statusCode': 200,
-        'body': json.dumps(mst_ts)
-    }
+print(queue.url)
